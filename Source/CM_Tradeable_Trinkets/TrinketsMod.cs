94572b04fadba79b67cd6c733b7b408fea56b824
NathanHarbour <nathanharbour@gmail.com>
2021-02-24 19:42:10 -0600

    Initial submission

---
+++ Source/CM_Tradeable_Trinkets/TrinketsMod.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace CM_Tradeable_Trinkets
+{
+    public class TrinketsMod : Mod
+    {
+        private static TrinketsMod _instance;
+        public static TrinketsMod Instance => _instance;
+
+        public TrinketsMod(ModContentPack content) : base(content)
+        {
+            //var harmony = new Harmony("CM_Tradeable_Trinkets");
+            //harmony.PatchAll();
+
+            _instance = this;
+        }
+    }
+}
